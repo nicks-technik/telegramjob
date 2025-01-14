059287ab84d793ed3721e27293f52de8440bb8c5
Nick <nick@nicks-technik.de>
2025-01-14 20:56:35 +0100

    working telegrampart for sending message and file.

--- telegrampart.py
+++ telegrampart.py
@@ -1,74 +1,38 @@
-from telegram import Update
-from dotenv import load_dotenv
 import os
-from telegram.ext import (
-    ApplicationBuilder,
-    ContextTypes,
-    CommandHandler,
-    MessageHandler,
-    filters,
-)
+from dotenv import load_dotenv
+from telethon.sync import TelegramClient, events
+import asyncio
 
 load_dotenv()
 
-TOKEN: str = os.getenv(key="TELEGRAM_BOT_TOKEN")
-SOURCE_CHAT_ID = os.getenv(key="SOURCE_CHAT_ID")
-DESTINATION_CHAT_ID = os.getenv(key="DESTINATION_CHAT_ID")
-SPECIFIC_TEXT: str = os.getenv(key="SPECIFIC_TEXT", default="video link")
+api_id: int = int(os.getenv(key="ENV_API_ID"))
+api_hash: str = os.getenv(key="ENV_API_HASH")
+destination_chat_id: int = int(os.getenv("ENV_DESTINATION_CHAT_ID"))
+# destination_chat_id: str = os.getenv("ENV_SOURCE_CHAT_ID")
 
-if not TOKEN:
-    raise Exception("TELEGRAM_BOT_TOKEN not found in environment variables")
-
-if not SOURCE_CHAT_ID:
-    raise Exception("SOURCE_CHAT_ID not found in environment variables")
-else:
-    SOURCE_CHAT_ID = int(SOURCE_CHAT_ID)
-
-if not DESTINATION_CHAT_ID:
-    raise Exception("DESTINATION_CHAT_ID not found in environment variables")
-else:
-    DESTINATION_CHAT_ID = int(DESTINATION_CHAT_ID)
-
-print(f"Bot Token: {TOKEN}")
-print(f"Source Chat ID: {SOURCE_CHAT_ID}")
-print(f"Destination Chat ID: {DESTINATION_CHAT_ID}")
-print(f"Specific Text: {SPECIFIC_TEXT}")
+client = TelegramClient("telegram", api_id, api_hash)
 
 
-async def message_handler(update: Update):
-    """
-    Handle incoming messages from Telegram updates.
+async def main():
+    # with TelegramClient("name", api_id, api_hash) as client:
+    me = await client.get_me()
+    print(me.stringify())
 
-    Args:
-        update (Update): The incoming update from Telegram
+    await client.send_message(destination_chat_id, "Hellx, myself!")
+    # client.send_message("me", "Hello, myself!")
+    # print(client.download_profile_photo("me"))
 
-    Returns:
-        None
-    """
-    if update.effective_chat.id == SOURCE_CHAT_ID:
-        message = update.message
-        if message and message.text and SPECIFIC_TEXT in message.text:
-            print(message.text)
+    # "me" is a user object. You can pretty-print
+    # any Telegram object with the "stringify" method:
 
+    # @client.on(events.NewMessage(pattern="(?i).*Mission Nr."))
+    # async def handler(event):
+    # await event.reply("Hey!")
+    await client.send_file(destination_chat_id, "./NickTHorn.jpg", caption="Nick Thorn")
 
-def run_telegram_bot() -> None:
-    """
-    Initializes and runs a Telegram bot using the python-telegram-bot library.
-
-    This function performs the following steps:
-    1. Creates a new Application instance using the bot TOKEN
-    2. Adds a message handler that processes all text messages
-    3. Starts the bot in polling mode
-
-    The bot will continue running and processing messages until the program is terminated.
-
-    Returns:
-        None
-    """
-    app = ApplicationBuilder().token(TOKEN).build()
-    app.add_handler(MessageHandler(filters.TEXT, message_handler))
-    app.run_polling()
+    client.run_until_disconnected()
 
 
 if __name__ == "__main__":
-    run_telegram_bot()
+    with client:
+        client.loop.run_until_complete(main())
