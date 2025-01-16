c93891001a455843e28bad2b5a3e8fa7380e192e
Nick <nick@nicks-technik.de>
2025-01-16 03:45:48 +0100

    commented the cd /home/nick/dev/telegramjob out, because it is made in the cron job

--- telegramjob.sh
+++ telegramjob.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-foldername="/home/nick/dev/telegramjob"
-cd $foldername
+# foldername="/home/nick/dev/telegramjob"
+# cd $foldername
 source ./.venv/bin/activate
 # /home/nick/dev/telegramjob/.venv/bin/python3 /home/nick/dev/telegramjob/telegramjob.py | tee /home/nick/dev/telegramjob/cron.log 2>&1
 # while true; do
