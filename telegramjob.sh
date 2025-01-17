1af4cc630986dd68524156559777f4cadcedc2f6
Nick <nick@nicks-technik.de>
2025-01-17 13:28:01 +0100

    no more logging via tee

--- telegramjob.sh
+++ telegramjob.sh
@@ -5,7 +5,8 @@
 source ./.venv/bin/activate
 # /home/nick/dev/telegramjob/.venv/bin/python3 /home/nick/dev/telegramjob/telegramjob.py | tee /home/nick/dev/telegramjob/cron.log 2>&1
 # while true; do
-python3 ./telegramjob.py | tee ./cron.log 2>&1
+#python3 ./telegramjob.py | tee ./cron.log 2>&1
+python3 ./telegramjob.py
 #     sleep 900 # wait for 10 minutes
 # done
 
