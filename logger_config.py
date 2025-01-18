515b146009169dff6c89b19c14dc6fd4abf61a6f
Nick <nick@nicks-technik.de>
2025-01-18 03:21:34 +0100

    added function to logging

--- logger_config.py
+++ logger_config.py
@@ -7,7 +7,7 @@ def setup_logger(log_file="app.log", console_level=logging.WARNING):
     logger.setLevel(logging.DEBUG)
 
     formatter = logging.Formatter(
-        "%(asctime)s - %(levelname)s - %(filename)s - %(lineno)d - %(message)s",
+        "%(asctime)s-%(levelname)s-%(filename)s-%(funcName)s-%(lineno)d- %(message)s",
         datefmt="%d.%m.%y %I:%M:%S",
     )
 
