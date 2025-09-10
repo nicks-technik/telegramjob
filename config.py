e8fa2ced8d07dcd9d0fd38b9790b757ebb866a26
Nick <nick@nicks-technik.de>
2025-09-11 00:11:04 +0200

    added two new secrets vars

--- config.py
+++ config.py
@@ -31,11 +31,20 @@ class Config:
         Config.SOURCE_CHAT_ID = int(os.getenv("ENV_SOURCE_CHAT_ID", default="0"))
         Config.WAIT_MIN = int(os.getenv("ENV_WAIT_MIN", default="60"))
         Config.WAIT_MAX = int(os.getenv("ENV_WAIT_MAX", default="300"))
+        print(f"WAIT_MAX: {Config.WAIT_MAX}")
         Config.TELEGRAM_LIMIT = int(os.getenv("ENV_TELEGRAM_LIMIT", default="100"))
         Config.HEADLESS = (
             os.getenv(key="ENV_HEADLESS", default="False").lower() == "true"
         )
-        Config.AUTH_FILE = os.getenv(key="ENV_AUTH_FILE", default="credentials").lower()
+        # Config.AUTH_FILE = os.getenv(key="ENV_AUTH_FILE", default="credentials").lower()
+        Config.CLIENT_SECRETS_FILE = os.getenv(
+            key="ENV_CLIENT_SECRETS_FILE", default="client_secret.json"
+        )
+        print(f"Client Secrets File: {Config.CLIENT_SECRETS_FILE}")
+        Config.STORAGE_STATE_PATH = os.getenv(
+            key="ENV_STORAGE_STATE_PATH", default="youtube_state.json"
+        )
+
         Config.SPECIFIC_TEXTS = os.getenv(key="ENV_SPECIFIC_TEXTS", default="").split(
             ","
         )
@@ -52,5 +61,7 @@ class Config:
     WAIT_MAX: int = 0
     TELEGRAM_LIMIT: int = 0
     HEADLESS: bool = False
-    AUTH_FILE: str = ""  # New: path to Playwright auth file
+    # AUTH_FILE: str = ""  # New: path to Playwright auth file
+    CLIENT_SECRETS_FILE = ""
+    STORAGE_STATE_PATH = ""
     SPECIFIC_TEXTS: list[str] = []
